5458feef1a0586939bc7f66ef88155216fc3c9cb
Tolga Nacar <83028055+tolganacar@users.noreply.github.com>
2023-04-27 13:45:11 +0300

    Designed register page and login page

--- AndroidVotingSystem/app/src/main/java/com/tolganacar/androidvotingsystem/view/loginpage/LoginPageFragment.kt
+++ AndroidVotingSystem/app/src/main/java/com/tolganacar/androidvotingsystem/view/loginpage/LoginPageFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.tolganacar.androidvotingsystem.R
+import com.tolganacar.androidvotingsystem.view.registerpage.RegisterPageFragmentDirections
+import kotlinx.android.synthetic.main.fragment_login_page.*
 
 class LoginPageFragment : Fragment() {
 
@@ -14,6 +17,24 @@ class LoginPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_login_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loginRegisterButtonOnClickListener()
+    }
+
+    private fun loginRegisterButtonOnClickListener() {
+        buttonRegisterLoginPage.setOnClickListener {
+            val action = LoginPageFragmentDirections.actionLoginPageFragmentToRegisterPageFragment()
+            findNavController().navigate(action)
+        }
+
+        buttonLoginLoginPage.setOnClickListener {
+            val action = LoginPageFragmentDirections.actionLoginPageFragmentToVoteFragment()
+            findNavController().navigate(action)
+        }
     }
 
 }
--- AndroidVotingSystem/app/src/main/java/com/tolganacar/androidvotingsystem/view/registerpage/RegisterPageFragment.kt
+++ AndroidVotingSystem/app/src/main/java/com/tolganacar/androidvotingsystem/view/registerpage/RegisterPageFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.tolganacar.androidvotingsystem.R
+import com.tolganacar.androidvotingsystem.view.mainpage.MainPageFragmentDirections
+import kotlinx.android.synthetic.main.fragment_register_page.*
 
 class RegisterPageFragment : Fragment() {
 
@@ -14,6 +17,23 @@ class RegisterPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        registerLoginButtonOnClickListener()
+    }
+
+    private fun registerLoginButtonOnClickListener() {
+        buttonRegisterRegisterPage.setOnClickListener {
+
+        }
+
+        buttonLoginRegisterPage.setOnClickListener {
+            val action = RegisterPageFragmentDirections.actionRegisterPageFragmentToLoginPageFragment()
+            findNavController().navigate(action)
+        }
     }
 
 }
